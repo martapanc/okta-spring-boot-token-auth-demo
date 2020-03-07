58f6b4b3ac85009e28feaa0f3e5da41264753fab
Marta Pancaldi <marta.panc@gmail.com>
2020-03-07 12:30:39 +0000

    Setup test configuration

---
+++ src/test/java/com/okta/springboottokenauth/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.okta.springboottokenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
